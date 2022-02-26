69a81909c424bd7a52384af17740e8e415d20b2d
Lorry Wei <58151836+theOGcat@users.noreply.github.com>
2022-02-25 20:48:06 -0500

    Co-authored-by: CodeTalker <xiaoshuaigeng@gmail.com> Co-authored-by: Yuchen  <zz511751524@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 
 const app = express()
 const port = 3001
@@ -9,24 +9,32 @@ app.use(express.urlencoded({extended:true}))
 
 
 // setup connection to local mysql db
-const connection =  mysql.createConnection({
+const connection =  mysql.createPool({
   host: 'localhost',
   user:'root',
   database: 'smartCampus',
 })
 
 // test db connection status
-connection.query(
-  'show tables', 
-  (err, results, fields) => {
-    if(results){
-      console.log('Database connect successfully')
-    }
+const test_connection = async () =>  {
+  const result = await connection.query('show tables')
+  console.log(result)
+  
+}
 
-    if(err){
-      console.log(err)
-    }
-})
+// test_connection()
+
+// connection.query(
+//   'show tables', 
+//   (err, results, fields) => {
+//     if(results){
+//       console.log('Database connect successfully')
+//     }
+
+//     if(err){
+//       console.log(err)
+//     }
+// })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -39,7 +47,6 @@ app.get('/', (req, res) => {
  *  2. AddComment
  *  3. SearchUser/[UserID] SearchUser/7
  */
-
 
 // user regisration endpoint
 app.post('/SignUp', async (req, res) => {
@@ -59,11 +66,60 @@ app.post('/SignUp', async (req, res) => {
         }
   })
     
-  connection.query()
+
     res.sendStatus(201)
     // console.log(firstName);
   
 })
+
+app.post('/SignIn', async(req, res) => {
+
+  const email = req.body.email;
+  const password = req.body.Password;
+
+  //verify Sign-In
+  const results = await connection.execute(
+    'SELECT * FROM `User`  \
+    WHERE `Email` = ? AND `Password` = ?' , 
+    [email,password]
+    // (err, results) => {
+    //   console.log(results)
+    // }
+  )
+  console.log(results[0])
+
+  if(results[0].length){
+    res.json(result[0])
+  }else{
+    res.status(400).send("Email or Password Not Correct")
+  }
+})
+
+// localhost:3001/commentls/pageNum
+app.get('/commentls/:pageNum', async(req,res)=>{
+  const pageNum = req.params.pageNum;
+  const results = await connection.execute(
+    'SELECT * FROM comment WHERE `pageNum` = ?',
+    [pageNum]
+  )
+    console.log(results[0])
+
+})
+
+//Insert Comment into the Blog section
+app.post('/commentInsert', async(req, res) => {
+  const pageNum = req.body.pageNum;
+  const userId = req.body.UserId;
+  const content = req.body.content;
+  const results = await connection.execute(
+    'INSERT INTO comment (`userID`,`pageNum`,`content`) VALUES(?,?,?)', 
+    [userId,pageNum,content]
+  )
+
+  console.log(results)
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
